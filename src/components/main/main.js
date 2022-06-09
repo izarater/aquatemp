d0ea3e8d38306501c936251aa9ec51c3e8d0072e
Paul Cuéllar <pacuellarp@unal.edu.co>
2022-06-09 00:44:52 -0500

    Primer commit de prueba

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -4,10 +4,11 @@ import Button from '../button/button';
 
 function Main() {
   return (
+      //Un primer paso para el agua limpia en Colombia. 
+     //Da el siguiente. Monitorea en línea una fuente hídrica.
     <main className="main">
       <h2 className="description">
-      Un primer paso para el agua limpia en Colombia.
-      Da el siguiente. Monitorea en línea una fuente hídrica.
+        Mi loco eso ta jevi, pero no te pregunté.
       </h2>
     </main>
   );
